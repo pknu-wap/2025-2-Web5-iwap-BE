982aaf6f24d727fad1c9813679cd3d2b7faf9fa5
sinwon <365faith365@gmail.com>
2025-09-18 23:59:19 +0900

    [feat] /api/inside/ 엔드포인트 추가

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing import List
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/api/inside/")
+def num_pic(layers: List):
+    return {"layers": layers}
+
+@app.post("/api/inside/")
+def num_pic(num_image: UploadFile):
+    return {"filename": num_image.filename}
+    
