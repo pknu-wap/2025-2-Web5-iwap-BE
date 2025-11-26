a63f8dceb54fa41bb7c92b4273c9c1633f5a377a
squareCaaat <danny79052@gmail.com>
2025-11-26 13:32:09 +0900

    fix(thisis4u): SMTP msg 본문 출력 제외

--- services/Thisis4u/postcard_mail.py
+++ services/Thisis4u/postcard_mail.py
@@ -253,8 +253,6 @@ def send_email(to_email: str, subject: str, html_body: str, text_body: str = Non
         msg.attach(MIMEText(text_body, "plain"))
     msg.attach(MIMEText(html_body, "html"))
 
-    print(msg.as_string())
-
     try:
         if use_ssl_tls:
             print("using ssl")
